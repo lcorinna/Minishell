1b1dc4023a1e0e65d23ebcb061e2402bbfacd326
Marylin Erlich <merlich@oa-d4.msk.21-school.ru>
2022-05-29 15:59:04 +0300

    my commit

--- libft/ft_lstnew_bonus.c
+++ libft/ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: merlich <merlich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 15:28:43 by merlich           #+#    #+#             */
-/*   Updated: 2022/05/27 22:44:13 by merlich          ###   ########.fr       */
+/*   Updated: 2022/05/28 19:57:04 by merlich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,16 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*new;
 
-	new = malloc(sizeof(t_list));
-	if (NULL == new)
+	new = NULL;
+	if (content != NULL)
 	{
-		exit(ENOMEM);
+		new = malloc(sizeof(t_list));
+		if (NULL == new)
+		{
+			exit(ENOMEM);
+		}
+		new->content = content;
+		new->next = NULL;
 	}
-	new->content = content;
-	new->next = NULL;
 	return (new);
 }
